584c89e2ffb1f41b41952156570a7a37b7b5d711
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-10-23 20:59:56 +0700

    fix path file in products data

--- js/homeProduct.js
+++ js/homeProduct.js
@@ -1,2 +1,27 @@
 import { products } from "../js/products.js";
-console.log(products)
+function get_homeProducts(){
+    let homeProducts = []
+    var cate = {"pants": 0, "jacket": 0, "shirt": 0, "dress": 0}
+    products.forEach(p =>{
+        if (cate[p.category] < 2){
+            homeProducts.push(p)
+            cate[p.category] +=1
+        }
+    })
+    return homeProducts
+}
+
+var homeProducts = get_homeProducts()
+function show_product(){
+    let i = 1
+    while (i<9){
+        document.getElementById(`Product${i}`).innerHTML= `
+            <img src=${homeProducts[i-1]["image"]}>
+            <h5 id="ProductName">${homeProducts[i-1]["name"]}</h5>
+            <p id="ProductPrice">${homeProducts[i-1]["price"]}</p>
+        `
+        i++;
+    }
+}
+show_product()
+
