f25fd82f4b919d6df91b62ec4d9fdc704555e335
thinhpt123 <152521888+thinhpt123@users.noreply.github.com>
2025-10-25 16:45:29 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,236 +1,262 @@
 export const products = [
-    {
-        "category": "pants",
-        "id": "pants1",
-        "name": "Quần dài ống suông vải sọc",
-        "price": 199000,
-        "image": "../img/pants/pants1.jpg",
-        "description": "Quần dài ống suông làm từ vải sọc nhẹ, phù hợp cho phong cách công sở thanh lịch.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants2",
-        "name": "Quần dài ống đứng lưng liền đính nơ hông",
-        "price": 349000,
-        "image": "../img/pants/pants2.jpg",
-        "description": "Thiết kế lưng liền tinh tế, điểm nhấn nơ hông tạo nét nữ tính, phù hợp đi làm và dạo phố.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants3",
-        "name": "Quần dài ống rộng lưng thun",
-        "price": 355000,
-        "image": "../img/pants/pants3.jpg",
-        "description": "Quần lưng thun co giãn, thoải mái khi mặc, kiểu dáng ống rộng giúp tôn dáng và dễ phối đồ.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants4",
-        "name": "Quần short linen túi xéo nhấn li trước",
-        "price": 295000,
-        "image": "../img/pants/pants4.jpg",
-        "description": "Chất liệu linen thoáng mát, thiết kế túi xéo và li trước tạo điểm nhấn trẻ trung.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants5",
-        "name": "Quần short lai cong 2 túi đính nơ",
-        "price": 346000,
-        "image": "../img/pants/pants5.jpg",
-        "description": "Kiểu dáng lai cong tinh tế, hai túi nhỏ có nơ xinh xắn mang lại vẻ nữ tính và năng động.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants6",
-        "name": "Quần thun ống suông đính hàng nơ",
-        "price": 149000,
-        "image": "../img/pants/pants6.jpg",
-        "description": "Chất thun mềm mại, nhẹ, hàng nơ trang trí giúp sản phẩm thêm phần đáng yêu và thoải mái.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "pants",
-        "id": "pants7",
-        "name": "Quần tây ống suông luồn dây lưng gắn hạt gỗ",
-        "price": 525000,
-        "image": "../img/pants/pants7.jpg",
-        "description": "Thiết kế hiện đại kết hợp dây lưng gắn hạt gỗ độc đáo, thích hợp cho phong cách thời trang thanh lịch.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket1",
-        "name": "Áo khoác hoodie unisex",
-        "price": 459000,
-        "image": "../img/jacket/jacket1.jpg",
-        "description": "Áo khoác hoodie unisex trẻ trung, năng động, chất liệu nỉ cotton mềm mại, phù hợp cho cả nam và nữ.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket2",
-        "name": "Áo hoodie nỉ cotton cao cấp",
-        "price": 469000,
-        "image": "../img/jacket/jacket2.jpg",
-        "description": "Áo hoodie form rộng, vải nỉ cotton cao cấp, ấm áp và thoải mái, dễ phối với nhiều phong cách.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket3",
-        "name": "Áo khoác khóa kéo hai túi",
-        "price": 499000,
-        "image": "../img/jacket/jacket3.jpg",
-        "description": "Áo khoác polyester thoáng khí, giữ ấm tốt, có túi hai bên và khóa kéo tiện lợi.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket4",
-        "name": "Áo khoác jeans unisex",
-        "price": 559000,
-        "image": "../img/jacket/jacket4.jpg",
-        "description": "Áo khoác denim bền đẹp, cổ bẻ, cài nút trước, dễ phối với nhiều trang phục cho phong cách cá tính.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket5",
-        "name": "Áo khoác gió chống nước nhẹ",
-        "price": 529000,
-        "image": "../img/jacket/jacket5.jpg",
-        "description": "Áo khoác gió polyester chống gió, kháng nước nhẹ, thiết kế năng động phù hợp cho hoạt động ngoài trời.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category": "jacket",
-        "id": "jacket6",
-        "name": "Áo khoác gió unisex form năng động",
-        "price": 525000,
-        "image": "../img/jacket/jacket6.jpg",
-        "description": "Áo khoác gió nhẹ, thoáng và chống nước, thích hợp cho đi học, đi chơi, hoặc du lịch.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"shirt",
-        "id": "shirt1",
-        "name": "Áo thun cotton mềm mịn",
-        "price": 199000,
-        "image": "../img/shirt/shirt1.jpg",
-        "description": "Vải cotton dày vừa, mềm mịn, thoáng khí, tạo cảm giác thoải mái khi mặc cả ngày.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"shirt",
-        "id": "shirt2",
-        "name": "Áo thun form regular cổ tròn",
-        "price": 189000,
-        "image": "../img/shirt/shirt2.jpg",
-        "description": "Form regular gọn gàng, cổ tròn bo đồng màu, dễ phối và phù hợp nhiều vóc dáng.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"shirt",
-        "id": "shirt3",
-        "name": "Áo thun cotton 220gsm thoáng khí",
-        "price": 219000,
-        "image": "../img/shirt/shirt3.jpg",
-        "description": "Chất liệu thun cotton 220gsm mềm mịn, bền đẹp, hạn chế nhăn, thoáng khí và thoải mái.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"shirt",
-        "id": "shirt4",
-        "name": "Áo thun oversize cổ tròn",
-        "price": 239000,
-        "image": "../img/shirt/shirt4.jpg",
-        "description": "Dáng oversize phóng khoáng, tay suông, cổ bo rib, dễ phối cùng jeans hoặc jogger.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"shirt",
-        "id": "shirt5",
-        "name": "Áo thun sọc ngang trắng xanh",
-        "price": 249000,
-        "image": "../img/shirt/shirt5.jpg",
-        "description": "Thiết kế sọc ngang hiện đại, in thấm sắc nét, logo số “19” tạo điểm nhấn năng động.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress1",
-        "name": "Đầm vải polyeste tái chế",
-        "price": 459000,
-        "image": "../img/dress/dress1.jpg",
-        "description": "Vải chính và lớp lót 100% polyeste tái chế, co giãn nhẹ, giặt máy nước lạnh, không sấy khô.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress2",
-        "name": "Đầm cotton pha elastan",
-        "price": 399000,
-        "image": "../img/dress/dress2.jpg",
-        "description": "Vải thân 98% bông, 2% elastan, thoải mái và dễ giặt, giữ dáng tốt sau nhiều lần sử dụng.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress3",
-        "name": "Đầm vải pha visco tái chế",
-        "price": 429000,
-        "image": "../img/dress/dress3.jpg",
-        "description": "Vải 37% bông, 36% polyeste, 27% visco, giặt nhẹ bằng máy, không giặt khô và không sấy.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress4",
-        "name": "Đầm cotton phối polyeste tái chế",
-        "price": 475000,
-        "image": "../img/dress/dress4.jpg",
-        "description": "Vải chính 100% bông, lớp lót polyeste tái chế, giặt khô hoặc giặt nhẹ, không sấy.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress5",
-        "name": "Đầm cotton phối polyeste",
-        "price": 495000,
-        "image": "../img/dress/dress5.jpg",
-        "description": "Vải chính 100% bông, lớp lót và túi polyeste, giặt khô hoặc giặt nhẹ, không sấy khô.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress6",
-        "name": "Đầm len pha polyeste tái chế",
-        "price": 515000,
-        "image": "../img/dress/dress6.jpg",
-        "description": "Vải 51% acrylic, 28% polyeste tái chế, 12% nylon, 7% len, giặt tay nước lạnh, không sấy.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress7",
-        "name": "Đầm visco lanh phối polyeste",
-        "price": 489000,
-        "image": "../img/dress/dress7.jpg",
-        "description": "Vải thân 43% visco, 34% lanh, 23% polyeste; váy lót polyeste tái chế; giặt khô hoặc giặt nhẹ.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    },
-    {
-        "category":"dress",
-        "id": "dress8",
-        "name": "Đầm polyeste tái chế 2 lớp",
-        "price": 459000,
-        "image": "../img/dress/dress8.jpg",
-        "description": "Vải thân và váy lót 100% polyeste tái chế, giặt nhẹ bằng máy, không sấy khô.",
-        "howtoclean": "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô"
-    }
-]
+  {
+    category: "pants",
+    id: "pants1",
+    name: "Quần dài ống suông vải sọc",
+    price: 199000,
+    image: "../img/pants/pants1.jpg",
+    description:
+      "Quần dài ống suông làm từ vải sọc nhẹ, phù hợp cho phong cách công sở thanh lịch.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants2",
+    name: "Quần dài ống đứng lưng liền đính nơ hông",
+    price: 349000,
+    image: "../img/pants/pants2.jpg",
+    description:
+      "Thiết kế lưng liền tinh tế, điểm nhấn nơ hông tạo nét nữ tính, phù hợp đi làm và dạo phố.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants3",
+    name: "Quần dài ống rộng lưng thun",
+    price: 355000,
+    image: "../img/pants/pants3.jpg",
+    description:
+      "Quần lưng thun co giãn, thoải mái khi mặc, kiểu dáng ống rộng giúp tôn dáng và dễ phối đồ.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants4",
+    name: "Quần short linen túi xéo nhấn li trước",
+    price: 295000,
+    image: "../img/pants/pants4.jpg",
+    description:
+      "Chất liệu linen thoáng mát, thiết kế túi xéo và li trước tạo điểm nhấn trẻ trung.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants5",
+    name: "Quần short lai cong 2 túi đính nơ",
+    price: 346000,
+    image: "../img/pants/pants5.jpg",
+    description:
+      "Kiểu dáng lai cong tinh tế, hai túi nhỏ có nơ xinh xắn mang lại vẻ nữ tính và năng động.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants6",
+    name: "Quần thun ống suông đính hàng nơ",
+    price: 149000,
+    image: "../img/pants/pants6.jpg",
+    description:
+      "Chất thun mềm mại, nhẹ, hàng nơ trang trí giúp sản phẩm thêm phần đáng yêu và thoải mái.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "pants",
+    id: "pants7",
+    name: "Quần tây ống suông luồn dây lưng gắn hạt gỗ",
+    price: 525000,
+    image: "../img/pants/pants7.jpg",
+    description:
+      "Thiết kế hiện đại kết hợp dây lưng gắn hạt gỗ độc đáo, thích hợp cho phong cách thời trang thanh lịch.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket1",
+    name: "Áo khoác hoodie unisex",
+    price: 459000,
+    image: "../img/jacket/jacket1.jpg",
+    description:
+      "Áo khoác hoodie unisex trẻ trung, năng động, chất liệu nỉ cotton mềm mại, phù hợp cho cả nam và nữ.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket2",
+    name: "Áo hoodie nỉ cotton cao cấp",
+    price: 469000,
+    image: "../img/jacket/jacket2.jpg",
+    description:
+      "Áo hoodie form rộng, vải nỉ cotton cao cấp, ấm áp và thoải mái, dễ phối với nhiều phong cách.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket3",
+    name: "Áo khoác khóa kéo hai túi",
+    price: 499000,
+    image: "../img/jacket/jacket3.jpg",
+    description:
+      "Áo khoác polyester thoáng khí, giữ ấm tốt, có túi hai bên và khóa kéo tiện lợi.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket4",
+    name: "Áo khoác jeans unisex",
+    price: 559000,
+    image: "../img/jacket/jacket4.jpg",
+    description:
+      "Áo khoác denim bền đẹp, cổ bẻ, cài nút trước, dễ phối với nhiều trang phục cho phong cách cá tính.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket5",
+    name: "Áo khoác gió chống nước nhẹ",
+    price: 529000,
+    image: "../img/jacket/jacket5.jpg",
+    description:
+      "Áo khoác gió polyester chống gió, kháng nước nhẹ, thiết kế năng động phù hợp cho hoạt động ngoài trời.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "jacket",
+    id: "jacket6",
+    name: "Áo khoác gió unisex form năng động",
+    price: 525000,
+    image: "../img/jacket/jacket6.jpg",
+    description:
+      "Áo khoác gió nhẹ, thoáng và chống nước, thích hợp cho đi học, đi chơi, hoặc du lịch.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "shirt",
+    id: "shirt1",
+    name: "Áo thun cotton mềm mịn",
+    price: 199000,
+    image: "../img/shirt/shirt1.jpg",
+    description:
+      "Vải cotton dày vừa, mềm mịn, thoáng khí, tạo cảm giác thoải mái khi mặc cả ngày.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "shirt",
+    id: "shirt2",
+    name: "Áo thun form regular cổ tròn",
+    price: 189000,
+    image: "../img/shirt/shirt2.jpg",
+    description:
+      "Form regular gọn gàng, cổ tròn bo đồng màu, dễ phối và phù hợp nhiều vóc dáng.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "shirt",
+    id: "shirt3",
+    name: "Áo thun cotton 220gsm thoáng khí",
+    price: 219000,
+    image: "../img/shirt/shirt3.jpg",
+    description:
+      "Chất liệu thun cotton 220gsm mềm mịn, bền đẹp, hạn chế nhăn, thoáng khí và thoải mái.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "shirt",
+    id: "shirt4",
+    name: "Áo thun oversize cổ tròn",
+    price: 239000,
+    image: "../img/shirt/shirt4.jpg",
+    description:
+      "Dáng oversize phóng khoáng, tay suông, cổ bo rib, dễ phối cùng jeans hoặc jogger.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "shirt",
+    id: "shirt5",
+    name: "Áo thun sọc ngang trắng xanh",
+    price: 249000,
+    image: "../img/shirt/shirt5.jpg",
+    description:
+      "Thiết kế sọc ngang hiện đại, in thấm sắc nét, logo số “19” tạo điểm nhấn năng động.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress1",
+    name: "Đầm vải polyeste tái chế",
+    price: 459000,
+    image: "../img/dress/dress1.jpg",
+    description:
+      "Vải chính và lớp lót 100% polyeste tái chế, co giãn nhẹ, giặt máy nước lạnh, không sấy khô.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress2",
+    name: "Đầm cotton pha elastan",
+    price: 399000,
+    image: "../img/dress/dress2.jpg",
+    description:
+      "Vải thân 98% bông, 2% elastan, thoải mái và dễ giặt, giữ dáng tốt sau nhiều lần sử dụng.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress3",
+    name: "Đầm vải pha visco tái chế",
+    price: 429000,
+    image: "../img/dress/dress3.jpg",
+    description:
+      "Vải 37% bông, 36% polyeste, 27% visco, giặt nhẹ bằng máy, không giặt khô và không sấy.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress4",
+    name: "Đầm cotton phối polyeste tái chế",
+    price: 475000,
+    image: "../img/dress/dress4.jpg",
+    description:
+      "Vải chính 100% bông, lớp lót polyeste tái chế, giặt khô hoặc giặt nhẹ, không sấy.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress5",
+    name: "Đầm cotton phối polyeste",
+    price: 495000,
+    image: "../img/dress/dress5.jpg",
+    description:
+      "Vải chính 100% bông, lớp lót và túi polyeste, giặt khô hoặc giặt nhẹ, không sấy khô.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress6",
+    name: "Đầm len pha polyeste tái chế",
+    price: 515000,
+    image: "../img/dress/dress6.jpg",
+    description:
+      "Vải 51% acrylic, 28% polyeste tái chế, 12% nylon, 7% len, giặt tay nước lạnh, không sấy.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress7",
+    name: "Đầm visco lanh phối polyeste",
+    price: 489000,
+    image: "../img/dress/dress7.jpg",
+    description:
+      "Vải thân 43% visco, 34% lanh, 23% polyeste; váy lót polyeste tái chế; giặt khô hoặc giặt nhẹ.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+  {
+    category: "dress",
+    id: "dress8",
+    name: "Đầm polyeste tái chế 2 lớp",
+    price: 459000,
+    image: "../img/dress/dress8.jpg",
+    description:
+      "Vải thân và váy lót 100% polyeste tái chế, giặt nhẹ bằng máy, không sấy khô.",
+    howtoclean: "Giặt máy nước lạnh, giặt nhẹ, Không giặt khô, Không sấy khô",
+  },
+];
