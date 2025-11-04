56a76fe15ba33b704febc57f23fa388e45858400
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-11-04 09:01:21 +0700

    update cart (2)

--- js/cart/checkout.js
+++ js/cart/checkout.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         );
 
         if (confirmCheckout) {
-            localStorage.removeItem("cart");
+            localStorage.removeItem(`${currentUser['ID']}cart`);
 
 
             alert("Thanh toán thành công! Cảm ơn bạn đã mua hàng.");
